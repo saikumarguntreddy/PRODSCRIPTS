58af6803c11cbd659a4c4419383e93ea4fff91e0
saikumarguntreddy <cd>
2023-05-21 02:10:51 +0530

    sai

---
+++ add.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Adition program"
+c=`expr $1 + $2`
+echo "value=$c"
---
+++ script-arg.sh
@@ -0,0 +1,21 @@
+echo " script argument list "
+echo
+echo "\$* value = $*"
+echo
+echo "\$@ value = $@"
+echo
+echo "\$0 value = $0"
+echo
+echo "\$1 value = $1"
+echo
+echo "\$2 value = $2"
+echo
+echo "\$3 value = $3"
+echo
+echo "\$4 value = $4"
+echo
+echo "\$5 value = $5"
+echo
+echo "\$# value = $#"
+echo
+echo "\$$ value = $$"
